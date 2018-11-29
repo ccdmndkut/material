365c76bec35b488318bbc361ce846822f77f16e0
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-11-29 18:05:11 -0500

    done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,73 @@
 <template>
-  <div id="app">
-    test
-    <HelloWorld/>
+  <div id='app'>
+    <mdc-layout-grid>
+      <mdc-layout-cell
+        align='middle'
+        desktop='12'
+        tablet='3'
+      >
+        <mdc-layout-inner-grid>
+          <mdc-layout-cell
+            desktop='2'
+            tablet='3'
+          ></mdc-layout-cell>
+          <mdc-layout-cell
+            class='mid'
+            desktop='8'
+            tablet='3'
+          >
+            <mdc-layout-inner-grid>
+              <mdc-layout-cell
+                :desktop='space'
+                tablet='3'
+              ></mdc-layout-cell>
+
+              <mdc-layout-cell
+                :desktop='desktop'
+                tablet='3'
+              >
+                <div @click='test'>
+                  <HelloWorld/>
+                </div>
+              </mdc-layout-cell>
+            </mdc-layout-inner-grid>
+          </mdc-layout-cell>
+        </mdc-layout-inner-grid>
+      </mdc-layout-cell>
+    </mdc-layout-grid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      desktop: 10
+    };
+  },
+  computed: {
+    space() {
+      var half = this.desktop / 2;
+      var minus = 6 - half
+      return minus
+    }
+  },
+  methods: {
+    test() {
+      if (this.desktop == 10) {
+        this.desktop = 8;
+      } else {
+        this.desktop = 10;
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -21,6 +75,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 60px;
+
+  background-color: #f5f5f5;
+  height: 100vh;
+}
+.mid {
+  background-color: #f5f5f5;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,37 @@
 <template>
-  <div class="hello mdc-typography">
-<mdc-button @click="onClick" :disabled="buttonDisabled">click me</mdc-button>
+  <div>
+    <mdc-card>
+      <mdc-card-header
+        subtitle='subtitle here'
+        title='Title goes here'
+      ></mdc-card-header>
+      <mdc-card-primary-action>
+        <mdc-card-media :src='card'></mdc-card-media>
+      </mdc-card-primary-action>
+
+      <mdc-card-actions>
+        <mdc-card-action-buttons>
+          <mdc-card-action-button>ACTION</mdc-card-action-button>
+        </mdc-card-action-buttons>
+        <mdc-card-action-icons>
+          <mdc-card-action-icon icon='star'/>
+        </mdc-card-action-icons>
+      </mdc-card-actions>
+    </mdc-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
