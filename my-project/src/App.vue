2c734330058d7f37d784579332f10973731bde79
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-11-29 17:17:12 -0500

    webpack

---
+++ my-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    test
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
